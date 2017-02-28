c7b33a2e14b9744bbcd40d2938a4c3f623070ea5
Nicholas Robinson <nr124@hw.ac.uk>
2017-02-28 17:28:09 +0000

    Fixed old version

--- OpenCLTotientOld.c
+++ OpenCLTotientOld.c
@@ -6,8 +6,8 @@
 #include "simple.h"
 
 const char *KernelSource =                                 "\n"
-"long hcf(long x, long y) {                                 \n"
-"    long t;                                                \n"
+"unsigned long hcf(unsigned long x, unsigned long y) {      \n"
+"    unsigned long t;                                       \n"
 "    while (y != 0) {                                       \n"
 "        t = x % y;                                         \n"
 "        x = y;                                             \n"
@@ -15,11 +15,11 @@ const char *KernelSource =                                 "\n"
 "    }                                                      \n"
 "    return x;                                              \n"
 "}                                                          \n"
-"int relprime(long x, long y) {                             \n"
+"int relprime(unsigned long x, unsigned long y) {           \n"
 "    return hcf(x, y) == 1;                                 \n"
 "}                                                          \n"
-"long euler(long n) {                                       \n"
-"    long length, i;                                        \n"
+"unsigned long euler(unsigned long n) {                     \n"
+"    unsigned long length, i;                               \n"
 "    length = 0;                                            \n"
 "    for (i = 1; i < n; i++)                                \n"
 "        if (relprime(n, i))                                \n"
@@ -28,10 +28,10 @@ const char *KernelSource =                                 "\n"
 "}                                                          \n"
 "__kernel void totient(                                     \n"
 "   const unsigned int lower,                               \n"
-"   __global long* results)                                 \n"
+"   __global unsigned long* results)                        \n"
 "{                                                          \n"
 "   int i = get_global_id(0);                               \n"
-"   long res = euler(i+lower);                              \n"
+"   unsigned long res = euler(i+lower);                     \n"
 "   results[i] = res;                                       \n"
 "}                                                          \n"
 "\n";
@@ -41,8 +41,8 @@ struct timespec start, stop;
 
 // hcf x 0 = x
 // hcf x y = hcf y (rem x y)
-long hcf(long x, long y) {
-    long t;
+unsigned long hcf(unsigned long x, unsigned long y) {
+    unsigned long t;
 
     while (y != 0) {
         t = x % y;
@@ -53,13 +53,13 @@ long hcf(long x, long y) {
 }
 
 // relprime x y = hcf x y == 1
-int relprime(long x, long y) {
+int relprime(unsigned long x, unsigned long y) {
     return hcf(x, y) == 1;
 }
 
 // euler n = length (filter (relprime n) [1 .. n-1])
-long euler(long n) {
-    long length, i;
+unsigned long euler(unsigned long n) {
+    unsigned long length, i;
 
     length = 0;
     for (i = 1; i < n; i++)
@@ -69,26 +69,26 @@ long euler(long n) {
 }
 
 // sumTotient lower upper = sum (map euler [lower, lower+1 .. upper])
-long sumTotient(long lower, long upper) {
-    long sum, i;
+unsigned long sumTotient(unsigned long lower, unsigned long upper) {
+    unsigned long sum, i;
 
     sum = 0;
     for (i = lower; i <= upper; i++) {
-		long res = euler(i);
+		unsigned long res = euler(i);
         sum = sum + res;
 	}
     return sum;
 }
 
-long sumArray(long a[], int num_elements)
+unsigned long sumArray(unsigned long a[], int num_elements)
 {
-   long i, sum=0;
+   unsigned long i, sum=0;
    for (i=0; i<num_elements; i++)
    {
 	 sum = sum + a[i];
    }
    return(sum);
-} 
+}
 
 void printTimeElapsed( char *text)
 {
@@ -100,7 +100,7 @@ void printTimeElapsed( char *text)
 void timeDirectImplementation( int lower, int upper)
 {
     clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start);
-    long sum = sumTotient(lower, upper);
+    unsigned long sum = sumTotient(lower, upper);
     clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &stop);
     printf("Result: %ld\n",sum);
     printTimeElapsed( "kernel equivalent on host");
@@ -115,7 +115,7 @@ int main (int argc, char * argv[])
     cl_kernel kernel;
     size_t global[1];
     size_t local[1];
-    
+
     if (argc < 2) {
 		lower = 1;
 		upper = 30;
@@ -139,15 +139,10 @@ int main (int argc, char * argv[])
     clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start);
 
     /* Create data for the run.    */
-    long *results = NULL;  /* Results returned from device.         */
+    unsigned long *results = NULL;  /* Results returned from device.         */
     int count = (upper - lower) + 1;
     global[0] = count;
-	//local[0] = hcf(count, 20);
-    results = (long *) malloc (count * sizeof (long));
-
-    /* Fill the vector with random float values.    */
-    for (int i = 0; i < count; i++)
-        results[i] = -1;
+    results = (unsigned long *) malloc (count * sizeof (unsigned long));
 
     err = initGPU();
 
@@ -157,32 +152,12 @@ int main (int argc, char * argv[])
                                                           LongArr, count, results);
 		printf("Run Kernel\n");
         runKernel( kernel, 1, global, local);
-        long result = sumArray(results, count);
+        unsigned long result = sumArray(results, count);
         clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &stop);
         printf("Result: %ld\n", result);
-        for (int i = 0; i < count; i++) {
-			//printf("results[%d]: %ld\n", i, results[i]);
-			if (results[i == -1])
-				break;
-		}
-        //printf("results[0]: %ld\n", results[0]);
-        //printf("results[1]: %ld\n", results[1]);
-        //printf("results[2]: %ld\n", results[2]);
-        //printf("results[3]: %ld\n", results[3]);
-        //printf("results[4]: %ld\n", results[4]);
 
         printKernelTime();
         printTimeElapsed( "CPU time spent");
-
-        /* Validate our results.    */
-        //correct = 0;
-        //for (int i = 0; i < count; i++)
-        //    if (results[i] == data[i] * data[i])
-        //        correct++;
-
-        /* Print a brief summary detailing the results.    */
-        //printf ("Computed %d/%d %2.0f%% correct values\n", correct, count,
-        //                (float)count/correct*100.f);
 
         err = clReleaseKernel (kernel);
         err = freeDevice();
